641ce5a748ea79be9f285f9354dc03f018e42bdd
Sri Subathra Devi B <subathrad18@gmail.com>
2021-07-16 13:33:37 +0530

    Create player.py

---
+++ code/player.py
@@ -0,0 +1,18 @@
+class Player:
+    def __init__(self,id):
+        self._id=id
+    
+    def get_id(self):
+        return self._id
+    
+    def get_color(self):
+        if self._id==0:
+            return (255,0,0)
+        else:
+            return (255,255,0)
+    
+    def get_name(self):
+        if self._id==0:
+            return "Player_1" #Red
+        else:
+            return "Player_2" #Yellow
